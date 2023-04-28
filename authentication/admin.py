b50f9055f94fb01efcdeccae0522e249caa1fab4
danielerat <davidodo2015@gmail.com>
2023-04-28 12:23:38 +0200

    Registering all admin models to the admin section

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Profile, Address, UserToken, Reset
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(UserToken)
+admin.site.register(Reset)
--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Cohort, Stack, Organization, OrganizationPosition, Trainer, Trainee
 # Register your models here.
+admin.site.register(Cohort)
+admin.site.register(Stack)
+admin.site.register(Organization)
+admin.site.register(OrganizationPosition)
+admin.site.register(Trainer)
+admin.site.register(Trainee)
