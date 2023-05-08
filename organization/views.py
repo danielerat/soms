d2175aa9044eb0ada5b2d6b671c06dd4205d65a7
danielerat <davidodo2015@gmail.com>
2023-05-08 21:32:12 +0200

    Registering the view for the stack endpoint

--- organization/views.py
+++ organization/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from organization.models import Cohort, Organization
-from organization.serializers import CohortSerializer, OrganizationSerializer
+from organization.models import Cohort, Organization, Stack
+from organization.serializers import CohortSerializer, OrganizationSerializer, StackSerializer
 # Create your views here.
 
 
@@ -18,4 +18,14 @@ class CohortViewset(ModelViewSet):
 
     def get_serializer_context(self):
         # Get the image id and send it in the backend
+        return {"organization_pk": self.kwargs["organization_pk"]}
+
+
+class StackViewset(ModelViewSet):
+    serializer_class = StackSerializer
+
+    def get_queryset(self):
+        return Stack.objects.filter(organization_id=self.kwargs['organization_pk'])
+
+    def get_serializer_context(self):
         return {"organization_pk": self.kwargs["organization_pk"]}
