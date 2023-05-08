9bee6d8f493f463e51db6ef2a3b7f4499e68e533
danielerat <davidodo2015@gmail.com>
2023-05-08 20:50:25 +0200

    Creating the endpoint for applicats to register

--- recruitment/views.py
+++ recruitment/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from recruitment.serializers import ApplySerializer
+from recruitment.models import Application
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+
+class ApplicationViewset(ModelViewSet):
+
+    serializer_class = ApplySerializer
+    queryset = Application.objects.all()
+
+# Endpoint to receive applications from users.
+
+
+class ApplyAPIView(APIView):
+    authentication_classes = []
+
+    def post(self, request):
+        data = request.data
+        serializer = ApplySerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
