560261575f7718e4e3423af01fe38b23bb8d92db
danielerat <davidodo2015@gmail.com>
2023-05-08 20:52:03 +0200

    Creating the viewset for the organization

--- organization/views.py
+++ organization/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from organization.models import Organization
+from organization.serializers import OrganizationSerializer
 # Create your views here.
+
+
+class OrganizationViewset(ModelViewSet):
+    serializer_class = OrganizationSerializer
+    queryset = Organization.objects.all()
