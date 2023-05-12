8e760bbb63b58ac6e84170ac3b4b8163d1f3bf16
danielerat <davidodo2015@gmail.com>
2023-05-12 13:00:05 +0200

    Adding some validations to our serializer

--- recruitment/serializers.py
+++ recruitment/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from recruitment.models import Application
+
+import re
 
 
 class ApplySerializer(serializers.ModelSerializer):
@@ -17,3 +19,18 @@ class ApplySerializer(serializers.ModelSerializer):
             "district",
             "dob",
         ]
+
+    def validate(self, data):
+        if not re.match(r'^\d{10}$', data['phone_number']):
+            raise serializers.ValidationError('Phone Number is Invalid')
+        # Get the phone number since it's unique
+        applicant = Application.objects.filter(
+            phone_number=data['phone_number'])
+        if applicant.exists():
+            print(applicant.first())
+            if applicant.first().application_status == "D":
+                raise serializers.ValidationError(
+                    'You were Not Selected for this Cohort.')
+            raise serializers.ValidationError(
+                'Your Application is being reviewed.')
+        return data
